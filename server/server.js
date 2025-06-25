39159310274a65292654a5e51fc71e6911f7cfde
bizualem <bizualemabebe04@gmail.com>
2025-06-25 14:35:10 +0100

    connect to mongodb atlas

--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
-
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+//connection to mogodb
 
-app.get('/', (req, res,) => {
-res.send('welcome to eccomerce');
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the E-commerce API!');
 });
 
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() => console.log(`server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
