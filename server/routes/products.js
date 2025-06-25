32db39e0c608bf364bdc2bfb12b55197073a4bab
bizualem <bizualemabebe04@gmail.com>
2025-06-25 19:08:46 +0100

    fetch a single product by ID.

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
+const product = require('../models/Product');
 
+// get all products
 
 router.get('/', async (req, res) => {
 try {
@@ -13,6 +15,22 @@ catch(err) {
     res.status(500).json({message:'server error', error: err.message});
 
 }
+});
+
+// get single product by id
+router.get('/:id', async (req,res) => {
+
+try{
+const foundProduct =await product.findById(req.params.id);
+if (!product) {
+    return res.status(404).json({message:'product not found'});
+}
+res.json(foundProduct);
+
+} catch (err){
+res.status(500).json({message: 'server error', error: err.message});
+}
+
 });
 
 
