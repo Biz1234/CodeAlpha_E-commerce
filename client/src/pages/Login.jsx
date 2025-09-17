8e7116338f9c2a2671b549beabfa486206b4118c
bizualem <bizualemabebe04@gmail.com>
2025-09-17 19:40:14 +0100

    adding new css

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthProvider';
-// import '../styles/auth.css';
+ import '../styles/Auth.css';
 
 const Login = () => {
   const { login } = useContext(AuthContext);
